1affb75949c55b10c52fe8c9902b90e274e946f6
Cristian C. Castillo <cris.c.castillo1@gmail.com>
2020-08-09 22:48:16 -0700

    Added Power Switch Logic

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,4 +1,4 @@
-const COUNT = 'COUNT'
+const POWER = 'POWER'
 const QTOUCH = 'QTOUCH'
 const WTOUCH = 'WTOUCH'
 const ETOUCH = 'ETOUCH'
@@ -11,22 +11,82 @@ const CTOUCH = 'CTOUCH'
 
 const defaultState = {
     counter:0,
-    myDisplay:'FOO Stuff'
+    myDisplay:'FOO',
+    powerOn:false
 }
 const reducer = (state = defaultState,action) => {
 
-    switch(action.type){
-        case COUNT:
-            return{
-                ...state,
-                counter:state.counter+1
+    if(state.powerOn === true){
+        console.log('IS ON')
+        switch(action.type){
+            case POWER:{
+                return{
+                    ...state,
+                    powerOn:false
+                }
             }
-    
-        default:
-            return state
-
+            case QTOUCH:
+                console.log('thats q')
+                return{
+                    ...state,
+                    myDisplay:QTOUCH
+                }
+            case WTOUCH:
+                return{
+                    ...state,
+                    myDisplay:WTOUCH
+                }
+            case ETOUCH:
+                return{
+                    ...state,
+                    myDisplay:ETOUCH
+                }
+            case ATOUCH:
+                return{
+                    ...state,
+                    myDisplay:ATOUCH
+                }
+            case STOUCH:
+                return{
+                    ...state,
+                    myDisplay:STOUCH
+                }
+            case DTOUCH:
+                return{
+                    ...state,
+                    myDisplay:DTOUCH
+                }
+            case ZTOUCH:
+                return{
+                    ...state,
+                    myDisplay:ZTOUCH
+                }
+            case XTOUCH:
+                return{
+                    ...state,
+                    myDisplay:XTOUCH
+                }
+            case CTOUCH:
+                return{
+                    ...state,
+                    myDisplay:CTOUCH
+                }
+            default:
+                return state
+        }
+    }else{
+        switch(action.type){
+            case POWER:{
+                return{
+                    ...state,
+                    powerOn:true
+                }
+            } 
+            default:
+                return state
+        }
     }
 }
 
-
 export default reducer;
+
