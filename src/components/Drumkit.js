743e16f243261005d80acdfc19cce1d128e13f26
Cristian C. Castillo <cris.c.castillo1@gmail.com>
2020-08-10 21:19:17 -0700

    Updated user interface
    Sound enabled for most buttons and color change upon the press. Also the power button now works!

--- src/components/Drumkit.js
+++ src/components/Drumkit.js
@@ -4,10 +4,65 @@ import DrumkitStyle from './DrumkitStyle.css'
 import Audio from './Audio';
 
 class Drumkit extends React.Component{
+
+    /* Event Listener for background button to highlight upon click */
+    changeBackgroundHandler = () => {
+
+    let newBackground = []
+
+        for(let i = 0; i < document.getElementsByTagName('button').length;i++){
+            newBackground[i] = document.getElementsByTagName('button')[i];
+            newBackground[i].style.backgroundColor = '#a846a0'
+            newBackground[i].addEventListener('click',this.revertBackgroundColorHandler)
+        }
+    }
+
+    revertBackgroundColorHandler = () => {
     
+        let newBackground = []
+
+        for(let i = 0; i < document.getElementsByTagName('button').length;i++){
+            newBackground[i] = document.getElementsByTagName('button')[i];
+        }
+
+        for(let i = 0; i < document.getElementsByTagName('button').length;i++){
+            
+            if(newBackground[i] === newBackground[0]){
+                newBackground[i].addEventListener('click',foo)
+            }
+            else if(newBackground[i] === newBackground[1]){
+                newBackground[i].addEventListener('click',foo)
+            }
+            else if(newBackground[i] === newBackground[2]){
+                newBackground[i].addEventListener('click',foo)
+            }
+            else if(newBackground[i] === newBackground[3]){
+                newBackground[i].addEventListener('click',foo)
+            }
+            else if(newBackground[i] === newBackground[4]){
+                newBackground[i].addEventListener('click',foo)
+            }
+            else if(newBackground[i] === newBackground[5]){
+                newBackground[i].addEventListener('click',foo)
+            }
+            else if(newBackground[i] === newBackground[6]){
+                newBackground[i].addEventListener('click',foo)
+            }
+            else if(newBackground[i] === newBackground[7]){
+                newBackground[i].addEventListener('click',foo)
+            }
+            else if(newBackground[i] === newBackground[8]){
+                newBackground[i].addEventListener('click',foo)
+            }
+            /* Foo changes button back to normal color upon leaving click */
+            function foo(){
+                newBackground[i].style.backgroundColor = '#ef72e4'
+            }
+        }
+    }
 
     render() {
-        /* Keyboard press control drum*/
+        /* Mapping dispatch drum*/
         const qButton = this.props.qButton;
         const wButton = this.props.wButton;
         const eButton = this.props.eButton;
@@ -21,48 +76,52 @@ class Drumkit extends React.Component{
 
         return(
                 <div>
-                <div className = 'container-fluid'>
-                    <h1 className ='logo-drumkitfont'>The DrumKit Funk</h1>
-                </div>
-                <div className = 'container-fuild'>
-                    <div className ='row powerButton'>
-                        <div className ='col-3'>
-                        <h6>BANK</h6>
-                        <label className = 'switch'>
-                            <input type = 'checkbox' />
-                            <span className ='slider round' />
-                        </label>
-                        </div>
-                        <div className = 'col-3'>
-                            <h6>POWER</h6>
+                    <div className = 'container-fluid'>
+                        <h1 className ='logo-drumkitfont'>The DrumKit Funk</h1>
+                    </div>
+                     {/* Power & Bank Button */}
+                    <div className = 'container-fuild'>
+                        <div className ='row powerButton'>
+                            <div className ='col-3'>
+                            <h6>BANK</h6>
                             <label className = 'switch'>
                                 <input type = 'checkbox' />
                                 <span className ='slider round' />
                             </label>
+                            </div>
+                            <div className = 'col-3'>
+                                <h6>POWER</h6>
+                                <label className = 'switch'>
+                                    <input onClick = {this.props.powerButton} type = 'checkbox'/>
+                                    <span className ='slider round' />
+                                </label>
+                            </div>
+                        </div>
+                        {/* Display Panel of type of sound */}
+                        <div className='row'>
+                            <h3 className = 'displayBeat'>Display</h3>
+                        </div>
+                        {/* Audio Bar */}
+                        <div className ='row'>
+                            <h3 className='audioStyle'><Audio /></h3>
+                        </div>
+                        {/* Buttons */}
+                        <div className = 'row'>
+                            <div className='col-3'><button className ='buttonStyle' id='qBackground1' onMouseLeave= {this.changeBackgroundHandler} onMouseDown = {this.changeBackgroundHandler} onClick = {qButton}>Q</button></div>
+                            <div className='col-3'><button className ='buttonStyle' id='qBackground2' onMouseLeave= {this.changeBackgroundHandler} onMouseDown = {this.changeBackgroundHandler} onClick = {wButton}>W</button></div>
+                            <div className='col-3'><button className ='buttonStyle' id='qBackground3' onMouseLeave= {this.changeBackgroundHandler} onMouseDown = {this.changeBackgroundHandler} onClick = {eButton}>E</button></div>
+                            </div>
+                            <div className = 'row'>
+                            <div className='col-3'><button className ='buttonStyle' id='qBackground4' onMouseLeave= {this.changeBackgroundHandler} onMouseDown = {this.changeBackgroundHandler} onClick = {aButton}>A</button></div>
+                            <div className='col-3'><button className ='buttonStyle' id='qBackground5' onMouseLeave= {this.changeBackgroundHandler} onMouseDown = {this.changeBackgroundHandler} onClick = {sButton}>S</button></div>
+                            <div className='col-3'><button className ='buttonStyle' id='qBackground6' onMouseLeave= {this.changeBackgroundHandler} onMouseDown = {this.changeBackgroundHandler} onClick = {dButton}>D</button></div>
+                        </div>
+                        <div className = 'row'>
+                            <div className='col-3'><button className ='buttonStyle' id='qBackground7' onMouseLeave= {this.changeBackgroundHandler} onMouseDown = {this.changeBackgroundHandler} onClick = {zButton}>Z</button></div>
+                            <div className='col-3'><button className ='buttonStyle' id='qBackground8' onMouseLeave= {this.changeBackgroundHandler} onMouseDown = {this.changeBackgroundHandler} onClick = {xButton}>X</button></div>
+                            <div className='col-3'><button className ='buttonStyle' id='qBackground9' onMouseLeave= {this.changeBackgroundHandler} onMouseDown = {this.changeBackgroundHandler} onClick = {cButton}>C</button></div>
                         </div>
                     </div>
-                    <div className='row'>
-                        <h3 className = 'displayBeat'>Display</h3>
-                    </div>
-                    <div className ='row'>
-                        <h3 className='audioStyle'><Audio /></h3>
-                    </div>
-                    <div className = 'row'>
-                        <div className='col-3'><button className ='buttonStyle' onClick = {qButton}>Q</button></div>
-                        <div className='col-3'><button className ='buttonStyle' onClick = {wButton}>W</button></div>
-                        <div className='col-3'><button className ='buttonStyle' onClick = {eButton}>E</button></div>
-                        </div>
-                    <div className = 'row'>
-                        <div className='col-3'><button className ='buttonStyle' onClick = {aButton}>A</button></div>
-                        <div className='col-3'><button className ='buttonStyle' onClick = {sButton}>S</button></div>
-                        <div className='col-3'><button className ='buttonStyle' onClick = {dButton}>D</button></div>
-                    </div>
-                    <div className = 'row'>
-                        <div className='col-3'><button className ='buttonStyle' onClick = {zButton}>Z</button></div>
-                        <div className='col-3'><button className ='buttonStyle' onClick = {xButton}>X</button></div>
-                        <div className='col-3'><button className ='buttonStyle' onClick = {cButton}>C</button></div>
-                    </div>
-                </div>
                 </div>
         )
     }
@@ -70,23 +129,24 @@ class Drumkit extends React.Component{
 
 const mapStateToProps = (state) => {
     return{
-        counterState:state.counter,
-        display:state.myDisplay
+        display:state.myDisplay,
+        powerButton:state.powerOn,
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return{
-        counterAction:()=> {dispatch({type:'COUNT'})},
-        qButton:()=> {dispatch({type:'QTOUCH'})},
-        wButton:()=> {dispatch({type:'WTOUCH'})},
-        eButton:()=> {dispatch({type:'ETOUCH'})},
-        aButton:()=> {dispatch({type:'ATOUCH'})},
-        sButton:()=> {dispatch({type:'STOUCH'})},
-        dButton:()=> {dispatch({type:'DTOUCH'})},
-        zButton:()=> {dispatch({type:'ZTOUCH'})},
-        xButton:()=> {dispatch({type:'XTOUCH'})},
-        cButton:()=> {dispatch({type:'CTOUCH'})},
+        qButton:() => dispatch({type:'QTOUCH'}),
+        wButton:() => dispatch({type:'WTOUCH'}),
+        eButton:() => dispatch({type:'ETOUCH'}),
+        aButton:() => dispatch({type:'ATOUCH'}),
+        sButton:() => dispatch({type:'STOUCH'}),
+        dButton:() => dispatch({type:'DTOUCH'}),
+        zButton:() => dispatch({type:'ZTOUCH'}),
+        xButton:() => dispatch({type:'XTOUCH'}),
+        cButton:() => dispatch({type:'CTOUCH'}),
+        powerButton:() => dispatch({type:'POWER'}),
+        changeBackgroundHandler:() => dispatch({type:'BACK'})
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,14 +10,14 @@ const XTOUCH = 'XTOUCH'
 const CTOUCH = 'CTOUCH'
 
 const defaultState = {
-    counter:0,
     myDisplay:'FOO',
-    powerOn:false
+    powerOn:false,
 }
+
 const reducer = (state = defaultState,action) => {
 
     if(state.powerOn === true){
-        console.log('IS ON')
+
         switch(action.type){
             case POWER:{
                 return{
@@ -26,32 +26,43 @@ const reducer = (state = defaultState,action) => {
                 }
             }
             case QTOUCH:
-                console.log('thats q')
+                let audioOne =  new Audio('/crash.mp3')
+                audioOne.play()
                 return{
                     ...state,
                     myDisplay:QTOUCH
                 }
             case WTOUCH:
+                const audioTwo = new Audio('/snare.mp3')
+                audioTwo.play()
                 return{
                     ...state,
                     myDisplay:WTOUCH
                 }
             case ETOUCH:
+                const audioThree = new Audio('/tom-1.mp3')
+                audioThree.play()
                 return{
                     ...state,
                     myDisplay:ETOUCH
                 }
             case ATOUCH:
+                const audioFour = new Audio('/tom-2.mp3')
+                audioFour.play()
                 return{
                     ...state,
                     myDisplay:ATOUCH
                 }
             case STOUCH:
+                const audioFive = new Audio('/tom-3.mp3')
+                audioFive.play()
                 return{
                     ...state,
                     myDisplay:STOUCH
                 }
             case DTOUCH:
+                const audioSix = new Audio('/tom-4.mp3')
+                audioSix.play()
                 return{
                     ...state,
                     myDisplay:DTOUCH
@@ -89,4 +100,3 @@ const reducer = (state = defaultState,action) => {
 }
 
 export default reducer;
-
